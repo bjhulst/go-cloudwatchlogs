71289a175bf3e6869df1c0f2799b5192f283688b
Nick Schuch <nick@myschuch.com>
2017-02-14 16:58:41 +1100

    Query all prefix logs

--- go-cloudwatch.go
+++ go-cloudwatch.go
@@ -1,7 +1,6 @@
 package cloudwatchlogs
 
 import (
-	"regexp"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -9,76 +8,76 @@ import (
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 )
 
-func GetLogs(region, group, stream, start, end string) (Logs, error) {
+func Events(region, group, stream, start, end string) (Logs, error) {
 	var logs Logs
 
-	diststart, err := time.ParseDuration(start)
+	svc := cloudwatchlogs.New(session.New(), &aws.Config{Region: aws.String(region)})
+
+	startDuration, err := time.ParseDuration(start)
 	if err != nil {
 		return logs, err
 	}
 
-	distend, err := time.ParseDuration(end)
+	endDuration, err := time.ParseDuration(end)
 	if err != nil {
 		return logs, err
 	}
 
 	var (
-		timefrom = aws.TimeUnixMilli(time.Now().Add(-diststart).UTC())
-		timeto   = aws.TimeUnixMilli(time.Now().Add(-distend).UTC())
+		from = aws.TimeUnixMilli(time.Now().Add(-startDuration).UTC())
+		to   = aws.TimeUnixMilli(time.Now().Add(-endDuration).UTC())
 	)
 
-	svc := cloudwatchlogs.New(session.New(), &aws.Config{Region: aws.String(region)})
-	resp, err := svc.GetLogEvents(&cloudwatchlogs.GetLogEventsInput{
-		LogGroupName:  aws.String(group),
-		LogStreamName: aws.String(stream),
-		StartFromHead: aws.Bool(true),
-		StartTime:     &timefrom,
-		EndTime:       &timeto,
-	})
-	if err != nil {
-		return logs, err
-	}
+	done := false
 
-	for _, e := range resp.Events {
-		logs = append(logs, &Log{
-			Stream:    stream,
-			Timestamp: time.Unix(*e.Timestamp/1000, 0),
-			Message:   *e.Message,
-		})
-	}
-
-	return logs, nil
-}
-
-func GetStreams(region, group, stream, start, end string) (Logs, error) {
-	var logs Logs
-
-	svc := cloudwatchlogs.New(session.New(), &aws.Config{Region: aws.String(region)})
 	params := &cloudwatchlogs.DescribeLogStreamsInput{
-		LogGroupName: aws.String(group),
-		Descending:   aws.Bool(true),
-		OrderBy:      aws.String("LogStreamName"),
-	}
-	resp, err := svc.DescribeLogStreams(params)
-	if err != nil {
-		return logs, err
+		LogGroupName:        aws.String(group),
+		LogStreamNamePrefix: aws.String(stream),
+		Descending:          aws.Bool(true),
 	}
 
-	r, err := regexp.Compile("(?i)" + stream + ".*")
-	if err != nil {
-		return logs, err
-	}
+	for !done {
+		resp, err := svc.DescribeLogStreams(params)
+		if err != nil {
+			return logs, err
+		}
 
-	for _, s := range resp.LogStreams {
-		if r.MatchString(*s.LogStreamName) {
-			newLogs, err := GetLogs(region, group, *s.LogStreamName, start, end)
+		for _, s := range resp.LogStreams {
+			// Ensure that we are not querying for streams which have finished prior to
+			if *s.LastEventTimestamp < from {
+				continue
+			}
+
+			var newLogs Logs
+
+			resp, err := svc.GetLogEvents(&cloudwatchlogs.GetLogEventsInput{
+				LogGroupName:  aws.String(group),
+				LogStreamName: s.LogStreamName,
+				StartFromHead: aws.Bool(true),
+				StartTime:     &from,
+				EndTime:       &to,
+			})
 			if err != nil {
 				return logs, err
+			}
+
+			for _, e := range resp.Events {
+				newLogs = append(newLogs, &Log{
+					Stream:    *s.LogStreamName,
+					Timestamp: time.Unix(*e.Timestamp/1000, 0),
+					Message:   *e.Message,
+				})
 			}
 
 			if len(newLogs) > 0 {
 				logs = MergeLogs(logs, newLogs)
 			}
+		}
+
+		if resp.NextToken == nil {
+			done = true
+		} else {
+			params.NextToken = resp.NextToken
 		}
 	}
 
