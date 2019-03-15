315522c7bf0382a1349ab2385a8451fdbcea71ed
Nick Schuch <nick@myschuch.com>
2019-03-15 11:32:59 +1000

    Fixes issue were all streams are listed before getting logging data

--- go-cloudwatch.go
+++ go-cloudwatch.go
@@ -1,75 +1,83 @@
 package cloudwatchlogs
 
 import (
-	"time"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 )
 
-func Events(region, group, stream, start, end string) (Logs, error) {
-	var logs Logs
-
-	svc := cloudwatchlogs.New(session.New(), &aws.Config{Region: aws.String(region)})
-
-	startDuration, err := time.ParseDuration(start)
-	if err != nil {
-		return logs, err
-	}
-
-	endDuration, err := time.ParseDuration(end)
-	if err != nil {
-		return logs, err
-	}
-
-	var (
-		from = aws.TimeUnixMilli(time.Now().Add(-startDuration).UTC())
-		to   = aws.TimeUnixMilli(time.Now().Add(-endDuration).UTC())
-	)
-
-	streams, err := streams(svc, group, stream)
-	if err != nil {
-		return logs, err
-	}
-
-	for _, s := range streams {
-		// Ensure that we are not querying for streams which have finished prior to
-		if s.LastEventTimestamp != nil && *s.LastEventTimestamp < from {
-			continue
-		}
-
-		newLogs, err := events(svc, group, *s.LogStreamName, from, to)
-		if err != nil {
-			return logs, err
-		}
-
-		if len(newLogs) > 0 {
-			logs = MergeLogs(logs, newLogs)
-		}
-	}
-
-	return logs, nil
+// QueryParams which get passed to the Query function.
+type QueryParams struct {
+	Group  string
+	Prefix string
+	Start  int64
+	End    int64
 }
 
-// Helper function to get Log Streams (with Token support).
-func streams(svc *cloudwatchlogs.CloudWatchLogs, group, prefix string) ([]*cloudwatchlogs.LogStream, error) {
-	var streams []*cloudwatchlogs.LogStream
+// QueryOutput which gets returned fromm Query function.
+type QueryOutput struct {
+	Logs []Log
+}
+
+// Log which contains messages from streams.
+type Log struct {
+	Stream    string
+	Timestamp int64
+	Message   string
+}
+
+// Query for logs.
+func Query(client *cloudwatchlogs.CloudWatchLogs, params QueryParams) (QueryOutput, error) {
+	var output QueryOutput
+
+	streams, err := getStreams(client, params.Group, params.Prefix, params.Start)
+	if err != nil {
+		return output, err
+	}
+
+	if len(streams) == 0 {
+		return output, nil
+	}
+
+	events, err := getLogs(client, params.Group, streams, params.Start, params.End)
+	if err != nil {
+		return output, err
+	}
+
+	for _, event := range events {
+		output.Logs = append(output.Logs, Log{
+			Stream:    *event.LogStreamName,
+			Timestamp: *event.Timestamp,
+			Message:   *event.Message,
+		})
+	}
+
+	return output, nil
+}
+
+// Helper function to get streams.
+func getStreams(client *cloudwatchlogs.CloudWatchLogs, group, prefix string, start int64) ([]*string, error) {
+	var streams []*string
 
 	params := &cloudwatchlogs.DescribeLogStreamsInput{
-		LogGroupName:        aws.String(group),
-		LogStreamNamePrefix: aws.String(prefix),
-		Descending:          aws.Bool(true),
+		LogGroupName: aws.String(group),
+		Descending:   aws.Bool(true),
+		OrderBy:      aws.String(cloudwatchlogs.OrderByLastEventTime),
 	}
 
 	for {
-		resp, err := svc.DescribeLogStreams(params)
+		resp, err := client.DescribeLogStreams(params)
 		if err != nil {
 			return streams, err
 		}
 
-		for _, s := range resp.LogStreams {
-			streams = append(streams, s)
+		for _, stream := range resp.LogStreams {
+			if *stream.LastEventTimestamp < start {
+				return streams, nil
+			}
+
+			streams = append(streams, stream.LogStreamName)
 		}
 
 		if resp.NextToken == nil {
@@ -78,45 +86,36 @@ func streams(svc *cloudwatchlogs.CloudWatchLogs, group, prefix string) ([]*cloud
 
 		params.NextToken = resp.NextToken
 	}
-
-	return streams, nil
 }
 
-// Helper function to get Events (with Token support).
-func events(svc *cloudwatchlogs.CloudWatchLogs, group, stream string, from, to int64) (Logs, error) {
-	var logs Logs
+// Helper function to get logs.
+func getLogs(client *cloudwatchlogs.CloudWatchLogs, group string, streams []*string, start, end int64) ([]*cloudwatchlogs.FilteredLogEvent, error) {
+	var events []*cloudwatchlogs.FilteredLogEvent
 
-	params := &cloudwatchlogs.GetLogEventsInput{
-		LogGroupName:  aws.String(group),
-		LogStreamName: aws.String(stream),
-		StartTime:     aws.Int64(from),
-		EndTime:       aws.Int64(to),
+	params := &cloudwatchlogs.FilterLogEventsInput{
+		LogGroupName:   aws.String(group),
+		LogStreamNames: streams,
+		StartTime:      &start,
+		EndTime:        &end,
+		Interleaved:    aws.Bool(true),
 	}
 
 	for {
-		resp, err := svc.GetLogEvents(params)
+		resp, err := client.FilterLogEvents(params)
 		if err != nil {
-			return logs, err
+			return events, err
 		}
 
-		if len(resp.Events) < 1 {
-			return logs, nil
+		fmt.Printf("Found %d events\n", len(resp.Events))
+
+		events = append(events, resp.Events...)
+
+		fmt.Println(resp.SearchedLogStreams)
+
+		if resp.NextToken == nil {
+			return events, nil
 		}
 
-		for _, e := range resp.Events {
-			logs = append(logs, &Log{
-				Stream:    stream,
-				Timestamp: time.Unix(*e.Timestamp/1000, 0),
-				Message:   *e.Message,
-			})
-		}
-
-		if resp.NextForwardToken == nil {
-			return logs, nil
-		}
-
-		params.NextToken = resp.NextForwardToken
+		params.NextToken = resp.NextToken
 	}
-
-	return logs, nil
 }
